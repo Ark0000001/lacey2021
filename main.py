f4cb68fd211d3a47c3a14a81b0f1e7286c008517
django <yotarona@gmail.com>
2021-09-01 20:28:40 +0500

    main

---
+++ main.py
@@ -0,0 +1,41 @@
+fio=input('fio:  ')
+print('hello', fio)
+
+print('what?\ngummy')
+
+a1=int(input('int1:  '))
+a2=int(input('int2:  '))
+answer=a1+a2
+print('summ: ', answer)
+
+a1=int(input('int1:  '))
+a2=int(input('int2:  '))
+a3=int(input('int3:  '))
+print('answer: ', (a1+a2)*a3)
+
+a1=int(input('pizza?:  '))
+a2=int(input('end?:  '))
+print('answer: ', a1-a2)
+
+name=input('name?:  ')
+a2=int(input('old?:  '))
+print(name,' in 2022 will be ', a2+1)
+
+a1=int(input('summ?:  '))
+a2=int(input('kol?:  '))
+print('$ on: ', a1/a2)
+
+a2=int(input('day?:  '))
+cl=a2*24
+minu=cl*60
+sek=minu*60
+print('clock: ', cl, '\nmin: ',minu,'\nsek: ',sek)
+
+a2=int(input('kg?:  '))
+funt=a2*2.204
+
+print('kg: ', a2, '\nfunt: ',funt)
+
+a1=int(input('>100?:  '))
+a2=int(input('10<?:  '))
+print('raz: ', a1//a2)
